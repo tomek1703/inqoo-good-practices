d97077a08eca14c1d222392de0b0a06f3b1b280b
adamkulawik <adamkulawik@wp.pl>
2020-10-10 16:01:52 +0100

    work in progress

---
+++ src/main/java/com/inqoo/quality/clean/bad/Calc.java
@@ -0,0 +1,25 @@
+package com.inqoo.quality.clean.bad;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class Calc {
+    public BigDecimal get(BigDecimal base) {
+        BigDecimal res;
+        if (base.doubleValue() <= 8000) {
+            res =  BigDecimal.ZERO;
+        } else if (base.doubleValue() >= 8001 && base.doubleValue() <= 13000) {
+            res =  BigDecimal.valueOf(base.doubleValue() * 0.17 -
+                    (1360 - 834.88 * (base.doubleValue() - 8000) / 5000)).setScale(0, RoundingMode.HALF_UP);
+        } else if (base.doubleValue() >= 13001 && base.doubleValue() <= 85528) {
+            res =  BigDecimal.valueOf(base.doubleValue() * 0.17 - 525.12).setScale(0, RoundingMode.HALF_UP);
+        } else if (base.doubleValue() >= 85529 && base.doubleValue() <= 127000) {
+            res =  BigDecimal.valueOf(85528 * 0.17 + (base.doubleValue() - 85528) * 0.32 - (525.12 - (525.12 * (base.doubleValue() - 85528)) / 41472)).setScale(0, RoundingMode.HALF_UP);
+        } else if (base.doubleValue() >= 12701 && base.doubleValue() < 1000000) {
+            res =  BigDecimal.valueOf(85528 * 0.17 + (base.doubleValue() - 85528) * 0.32).setScale(0, RoundingMode.HALF_UP);
+        } else {
+            res =  BigDecimal.valueOf(85528 * 0.17 + (base.doubleValue() - 85528) * 0.32 + (base.doubleValue() - 1000000) * 0.04).setScale(0, RoundingMode.HALF_UP);
+        }
+        return res;
+    }
+}
---
+++ src/test/java/com/inqoo/quality/clean/bad/TaxTest.java
@@ -0,0 +1,47 @@
+package com.inqoo.quality.clean.bad;
+
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.math.BigDecimal;
+
+import static junitparams.JUnitParamsRunner.$;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(JUnitParamsRunner.class)
+public class TaxTest {
+
+    @Test
+    @Parameters(method = "taxParams")
+    public void calculateTax(BigDecimal base, BigDecimal expectedTax) {
+        // given
+        Calc taxCalculator = new Calc();
+
+        // when
+        BigDecimal calculatedTax = taxCalculator.get(base);
+
+        // then
+        assertThat(calculatedTax).isEqualTo(expectedTax);
+    }
+
+    public Object taxParams() {
+        return $(
+                $(BigDecimal.valueOf(8000), BigDecimal.ZERO),
+                $(BigDecimal.valueOf(8001), BigDecimal.ZERO),
+                $(BigDecimal.valueOf(10000), BigDecimal.valueOf(674)),
+                $(BigDecimal.valueOf(13000), BigDecimal.valueOf(1685)),
+                $(BigDecimal.valueOf(13001), BigDecimal.valueOf(1685)),
+                $(BigDecimal.valueOf(85527), BigDecimal.valueOf(14014)),
+                $(BigDecimal.valueOf(85528), BigDecimal.valueOf(14015)),
+                $(BigDecimal.valueOf(85529), BigDecimal.valueOf(14015)),
+                $(BigDecimal.valueOf(127000), BigDecimal.valueOf(27811)),
+                $(BigDecimal.valueOf(127001), BigDecimal.valueOf(27811)),
+                $(BigDecimal.valueOf(999999), BigDecimal.valueOf(307170)),
+                $(BigDecimal.valueOf(1000000), BigDecimal.valueOf(307171)),
+                $(BigDecimal.valueOf(1000001), BigDecimal.valueOf(307171))
+        );
+
+    }
+}
