90f6cadacc55e13396aab8529392015d7799698b
adamkulawik <adamkulawik@wp.pl>
2020-10-11 01:09:21 +0100

    work in progress

--- src/main/java/com/inqoo/quality/clean/bad/Calc.java
+++ src/main/java/com/inqoo/quality/clean/bad/Calc.java
@@ -8,6 +8,7 @@ WTF?
 BIG FAT FAIL!
  */
 public class Calc {
+    // gets tax value for given base income
     public BigDecimal get(BigDecimal value) {
         BigDecimal res;
         if (value.doubleValue() <= 8000) {
@@ -16,6 +17,8 @@ public class Calc {
             res =  BigDecimal.valueOf(value.doubleValue() * 0.17 - (1360 - 834.88 * (value.doubleValue() - 8000) / 5000)).setScale(0, RoundingMode.HALF_UP);
         } else if (value.doubleValue() > 13000 && value.doubleValue() <= 85528) {
             res =  BigDecimal.valueOf(value.doubleValue() * 0.17 - 525.12).setScale(0, RoundingMode.HALF_UP);
+
+        // drugi próg
         } else if (value.doubleValue() > 85528 && value.doubleValue() <= 127000) {
             res =  BigDecimal.valueOf(85528 * 0.17 + (value.doubleValue() - 85528) * 0.32 - (525.12 - (525.12 * (value.doubleValue() - 85528)) / 41472)).setScale(0, RoundingMode.HALF_UP);
         } else if (value.doubleValue() > 127000 && value.doubleValue() < 1000000) {
